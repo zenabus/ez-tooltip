1d9c5d615042e36a8c18ffe1968415e3c4265f1a
Zenabus <foibanez@gmail.com>
2020-05-29 03:18:51 +0800

    v1.0.1

--- ez-tooltip.js
+++ ez-tooltip.js
@@ -1,3 +1,12 @@
+/*!
+ * EZ-Tooltip.js v1.0.1
+ *
+ * https://github.com/zenabus
+ *
+ * Copyright (c) 2020 Francisco Ibañez III
+ * Free to use under the MIT license.
+ */
+
 document.addEventListener("DOMContentLoaded", function () {
   const arrow = {
     top: function (val) {
@@ -29,8 +38,9 @@ document.addEventListener("DOMContentLoaded", function () {
     hide: function () {
       span.style.opacity = 0;
     },
-    text: function (tool) {
-      span.textContent = tool.getAttribute('data-tooltip');
+    text: function (text) {
+      // span.textContent = tool.getAttribute('data-tooltip');
+      span.textContent = text;
     },
     width: function () {
       return span.offsetWidth;
@@ -70,13 +80,12 @@ document.addEventListener("DOMContentLoaded", function () {
   for (const el of tooltips) {
     el.appendChild(span);
     el.addEventListener('mousemove', function (e) {
-      tooltip.text(el);
-      tooltip.show();
-
+      e.stopImmediatePropagation();
       const HEIGHT = e.clientY + tooltip.height();
       const WIDTH = e.clientX + tooltip.width();
       const AT_RIGHT = window.innerWidth < WIDTH + 25;
       const AT_BOTTOM = window.innerHeight < HEIGHT + 25;
+      const AT_TOP = e.clientY < tooltip.height() + 20;
 
       if (AT_BOTTOM && AT_RIGHT) {
         tooltip.topLeft(e);
@@ -84,10 +93,14 @@ document.addEventListener("DOMContentLoaded", function () {
         tooltip.topRight(e);
       } else if (AT_RIGHT) {
         tooltip.bottomLeft(e);
-      } else {
+      } else if(AT_TOP) {
         tooltip.bottomRight(e);
+      } else {
+        tooltip.topRight(e);
       }
 
+      tooltip.text(el.getAttribute('data-tooltip'));
+      tooltip.show();
     });
     el.addEventListener('mouseout', function () {
       tooltip.hide();
